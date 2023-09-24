5fdaf86d541bc495f8916d2433ebfd7438f2834e
Beaskyy <afeezakintobi@nqb8.co>
2023-09-24 20:23:29 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Box } from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
     </View>
   );
 }
 
+windowWidth = Dimensions.get("window").width
+windowHeight = Dimensions.get("window").height
+
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "plum",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    width: windowWidth >  500 ? "60%" : "90%",
+    height: windowHeight > 500 ? "40%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24
+  }
 });
